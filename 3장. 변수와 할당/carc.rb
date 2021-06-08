c1fbacd7b09c36b1680e20ed59b3e64db58970e2
snlcom <admin@adminui-MacBookPro.local>
2021-06-08 10:30:45 +0900

    3장. 변수와 할당

---
+++ 3장. 변수와 할당/carc.rb
@@ -0,0 +1,22 @@
+myString = "안녕하세요"
+
+puts myString
+puts myString
+
+name = 'Lee Inpyeong'
+puts 'My name is ' + name + '.'
+
+composer = "모자르트"
+puts composer + '는 "천재"여'
+
+var1 = 8
+var2 = var1
+
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
