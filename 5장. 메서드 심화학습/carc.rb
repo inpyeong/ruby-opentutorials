c8cf3f6cfe94511abe951ebfeb73473e00e84af9
snlcom <admin@adminui-MacBookPro.local>
2021-06-08 12:10:46 +0900

    5장, 메서드 심화학습

---
+++ 5장. 메서드 심화학습/carc.rb
@@ -0,0 +1,38 @@
+three = 3
+puts three
+# self.puts three
+
+var1 = 'inpyeong'
+puts var1.reverse
+puts var1
+
+name = 'inpyeong'
+
+puts name.length.to_s + 'str'
+
+
+
+
+
+
+linewidth = 50
+puts 'Old Mother Hubbard'.center(linewidth)
+
+
+linewidth = 25
+str = 'starbucks'
+puts str.rjust linewidth
+
+puts 5 ** 0.5
+
+
+
+puts rand(101)
+
+srand 123
+puts rand(100)
+
+srand 123
+puts rand(100)
+
+puts Math::PI
