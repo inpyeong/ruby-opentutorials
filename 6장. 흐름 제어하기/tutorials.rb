07c8e454ac96869d2d907f80b8768a7e833dce2a
snlcom <admin@adminui-MacBookPro.local>
2021-06-08 12:26:56 +0900

    6장. 흐름 제어하기

---
+++ 6장. 흐름 제어하기/tutorials.rb
@@ -0,0 +1,20 @@
+name = gets.chomp
+
+if name == 'inpyeong'
+  puts 'Hello sir'
+else
+  puts 'Who are you?'
+end
+
+command = ''
+
+# while command != 'bye'
+#   puts command
+#   command = gets.chomp
+# end
+# 
+# puts 'Yeah'
+
+if (name == 'inpyeong' or name == 'Inpyeong')
+  puts 'starbucks'
+end
