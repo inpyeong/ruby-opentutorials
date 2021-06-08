e63e765c0c930c0db5dcb847d593b70fb80fab19
snlcom <admin@adminui-MacBookPro.local>
2021-06-08 10:53:56 +0900

    4장. 함께 사용하기

---
+++ 4장. 함께 사용하기/carc.rb
@@ -0,0 +1,15 @@
+var1 = 2
+var2 = '5'
+
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+puts '내가 가장 좋아하는 수는 5다!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+
+# puts gets
+
+puts 'Hi, what\'s your name?'
+name = gets.chomp
+puts 'You\'re name is ' + name
+puts 'Nice to meet you, ' + name
