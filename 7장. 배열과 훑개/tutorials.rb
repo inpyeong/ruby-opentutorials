b3f2108ae118221dfd5013ee097f4dc4a8e40105
snlcom <admin@adminui-MacBookPro.local>
2021-06-08 14:03:37 +0900

    7장. 배열과 훑개

---
+++ 7장. 배열과 훑개/tutorials.rb
@@ -0,0 +1,32 @@
+arr = [123, '123', [true, false]]
+
+# puts arr
+
+arr.each do |a|
+  puts a
+end
+
+puts '=========='
+
+3.times do
+  puts 'ruby master'
+end
+
+puts '=========='
+
+puts arr.to_s
+puts
+puts arr.join(', ')
+
+puts '=========='
+
+favorites = []
+favorites.push 'basketball'
+favorites.push 'song'
+puts favorites
+puts '=========='
+
+favorites.pop
+puts favorites
+
+puts favorites.last
